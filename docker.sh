51887b19593f8664ec65157edc3e695c8ec61989
daniilshilo-developer <daniilshilo.developer@gmail.com>
2021-10-04 16:01:31 +0300

    Docker script

---
+++ docker.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Author: Daniil Shilo (daniilshilo-developer) <daniilshilo.developer@gmail.com>
+# Description: Installs Docker Engine on Fedora
+# Destination: This script will add Docker repository to local list of repositories and install docker-ce, docker-ce-cli containerd.io packages
+
+# Function for output messages
+function message() {
+	# Green bold text
+	echo -e '\033[1;32m'$1'\033[0m'
+}
+
+message 'Removing old Docker instances'
+sudo dnf remove docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-selinux \
+  docker-engine-selinux \
+  docker-engine
+
+message 'Adding Docker repositories to local list'
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+  --add-repo \
+  https://download.docker.com/linux/fedora/docker-ce.repo
+
+message 'Installing Docker on the machine'
+sudo dnf install -y docker-ce \
+  docker-ce-cli \
+  containerd.io
+
+message 'Starting Docker services'
+sudo systemctl enable --now docker
