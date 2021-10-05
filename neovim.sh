6a82f049206509f5bf678b016225b43f1c1f7994
daniilshilo-developer <daniilshilo.developer@gmail.com>
2021-10-06 00:06:30 +0300

    Conquer of Code added to Neovim configuration

--- neovim.sh
+++ neovim.sh
@@ -11,16 +11,21 @@ function message() {
 
 # Installing vim-plug
 message 'Installing vim-plug'
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+if [ ! -f '"${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim' ]; then
+	sh -c 'curl -sSfLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+
 
 message 'Installing some packages for vim-plug'
-sudo dnf install -y fzf nodejs npm
+sudo dnf install -y fzf nodejs npm python3-neovim
 # Writing configuration into $HOME/config/nvim/init.vim
 message 'Writing some configuration in $HOME/config/nvim/init.vim'
 mkdir -p $HOME/.config/nvim
 cat <<EOF >$HOME/.config/nvim/init.vim
 " # Plugins
 call plug#begin('~/.config/nvim/plugged')
+	" Conquer of Completion
+	Plug 'neoclide/coc.nvim', {'branch': 'release'}
 	" NERDTree file explorer
 	Plug 'scrooloose/nerdtree', { 'on':  'NERDTreeToggle' }
 	" FuzzyFinder (for quick search for files in project)
@@ -90,3 +95,4 @@ EOF
 # Install plugins
 message 'Installing plugins'
 nvim --headless +PlugInstall +qa
+nvim --headless +'CocInstall -sync coc-json coc-htmlhint coc-css coc-emmet coc-yaml coc-snippets coc-tailwindcss coc-tsserver coc-sh' +qa
